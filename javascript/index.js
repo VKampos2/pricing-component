e23f71e8401a3e14d8fb4f73d2ca0cd8a08d50ba
VKampos2 <105946892+VKampos2@users.noreply.github.com>
2022-07-07 00:16:34 +0500

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,6 @@
-function myfunction() {
-  let checkBox = document.getElementById("toggle");
-  if (checkBox.checked) {
-    document.getElementById("yearllyPrice").style.display = "none";
-    document.getElementById("yearllyPrice1").style.display = "none";
-    document.getElementById("yearllyPrice2").style.display = "none"
-    document.getElementById("monthlyPrice").style.display = "flex";
-    document.getElementById("monthlyPrice1").style.display = "flex";
-    document.getElementById("monthlyPrice2").style.display = "flex";
-  } else {
-    document.getElementById("yearllyPrice").style.display = "flex";
-    document.getElementById("yearllyPrice1").style.display = "flex";
-    document.getElementById("yearllyPrice2").style.display = "flex";
-    document.getElementById("monthlyPrice").style.display = "none";
-    document.getElementById("monthlyPrice1").style.display = "none";
-    document.getElementById("monthlyPrice2").style.display = "none";
-  }
+const checkBox=getElementById("toggle")
+const hiddenPrice=document.querySelectorAll('.price')
+const yearllyPrice=document.querySelectorAll('.yearlly-price')
 
+checkBox.addEventListener('click',()=> {hiddenPrice.forEach(el=>{el.classList.toggle('show');});yearPrice.forEach(el.classList.toggle('yearlly-price');});});
 
-}
